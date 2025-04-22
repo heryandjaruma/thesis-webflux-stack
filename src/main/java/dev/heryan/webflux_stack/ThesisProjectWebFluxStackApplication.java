d942a7a40f3ec464c7a06de9238f18d8e45d23c9
Heryan Djaruma <heryandjaruma@gmail.com>
2025-04-22 17:10:24 +0700

    enable mongo reactive

--- src/main/java/dev/heryan/webflux_stack/ThesisProjectWebFluxStackApplication.java
+++ src/main/java/dev/heryan/webflux_stack/ThesisProjectWebFluxStackApplication.java
@@ -2,8 +2,10 @@ package dev.heryan.webflux_stack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
+@EnableReactiveMongoRepositories
 public class ThesisProjectWebFluxStackApplication {
 
 	public static void main(String[] args) {
